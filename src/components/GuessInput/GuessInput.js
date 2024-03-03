a1774e69473a0bd62b55933a3adf1322220c2734
Prayatna Bhattarai <prayatna.bhattarai@gmail.com>
2024-03-03 12:25:24 +0800

    Add guess input component

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function GuessInput() {
+  const [guessWord, setGuessWord] = React.useState("");
+
+  function submitGuessWord(event) {
+    event.preventDefault();
+    console.log({ guessWord });
+    setGuessWord("");
+  }
+
+  return (
+    <form onSubmit={submitGuessWord} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        pattern="[a-zA-Z]{5}"
+        title="should be 5 letter word"
+        value={guessWord}
+        onChange={(event) => {
+          const nextGuessWord = event.target.value.toUpperCase();
+          setGuessWord(nextGuessWord);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
